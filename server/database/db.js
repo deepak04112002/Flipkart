c4173269b02f6c21ab2cf517ed99736229a81291
Your Name <you@example.com>
2023-09-09 20:40:32 +0530

    vercel

--- server/database/db.js
+++ server/database/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 
 export const Connection= async (USERNAME,PASSWORD) => {
-   const URL= `mongodb+srv://user:user@ecommerce-web.rewnl2a.mongodb.net/?retryWrites=true&w=majority`;
+   const URL= `mongodb+srv://${USERNAME}:${PASSWORD}@ecommerce-web.rewnl2a.mongodb.net/?retryWrites=true&w=majority`;
     try{
         await mongoose.connect(URL,{ useUnifiedTopology: true,useNewUrlParser: true});
         console.log("Database connected sucessfully");
