799d48908f8fcde66560f8a1ec1ee4f14afbec83
Your Name <you@example.com>
2023-09-09 22:34:27 +0530

    double cottation

--- client/src/redux/actions/productActions.js
+++ client/src/redux/actions/productActions.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 import * as actionTypes from '../constants/productConstant';
 
-const URL = 'https://flipkart-api-xi.vercel.app'
+const URL = "https://flipkart-api-xi.vercel.app"
 
 export const getProducts = () => async (dispatch) => {
     try {
