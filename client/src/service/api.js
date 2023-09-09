7a723f2b959a4a8f1c4e4436f06f9edb0307bd8f
Your Name <you@example.com>
2023-09-09 22:21:06 +0530

    single cottation

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const URL = "https://flipkart-api-xi.vercel.app";
+const URL = 'https://flipkart-api-xi.vercel.app';
+
 
 export const authenticateSignup = async (data) => {
   try {
